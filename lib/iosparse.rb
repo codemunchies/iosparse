f37cb0cf118b4b7a5f818d18fcd52cba5fc21ce6
codemunkii <codemunchies@gmail.com>
2013-07-21 14:53:03 -0400

    two space all of the things

--- lib/iosparse.rb
+++ lib/iosparse.rb
@@ -4,100 +4,100 @@ require 'yaml'
 #
 class IOSParse
 
-	def initialize(filename)
-		load_config(filename)
-	end
+  def initialize(filename)
+    load_config(filename)
+  end
 
-	# Custom filter
-	def find_all(custom_filter)
-		options = {
-			filter: /^#{custom_filter}.+/,
-			parent: "#{custom_filter}"
-		}
-		find_blocks(options)
-	end
+  # Custom filter
+  def find_all(custom_filter)
+    options = {
+      filter: /^#{custom_filter}.+/,
+      parent: "#{custom_filter}"
+    }
+    find_blocks(options)
+  end
 
-	# Custom seek
-	def has(custom_seek)
-		options = {
-			filter: /.+#{custom_seek}.+/,
-			parent: "#{custom_seek}"
-		}
-		find_blocks(options)
-	end
+  # Custom seek
+  def has(custom_seek)
+    options = {
+      filter: /.+#{custom_seek}.+/,
+      parent: "#{custom_seek}"
+    }
+    find_blocks(options)
+  end
 
-	# What group is an IP in?
-	def group_has_ip(ip)
-		options = {
-			filter: /^object-group.+#{ip}.+/,
-			parent: "#{ip}"
-		}
-		find_object_group(options)
-	end
+  # What group is an IP in?
+  def group_has_ip(ip)
+    options = {
+      filter: /^object-group.+#{ip}.+/,
+      parent: "#{ip}"
+    }
+    find_object_group(options)
+  end
 
-	private
+  private
 
-	def find_blocks(options)
-		output = Array.new
-		@yaml.each do |line|
-			output << line.match(options[:filter]) if line.match(options[:filter])
-		end
-		normalize(output)
-	end
+  def find_blocks(options)
+    output = Array.new
+    @yaml.each do |line|
+      output << line.match(options[:filter]) if line.match(options[:filter])
+    end
+    normalize(output)
+  end
 
-	def find_object_group(options)
-		input = Array.new
-		output = Array.new
-		@yaml.each do |line|
-			input << line.match(options[:filter]) if line.match(options[:filter])
-		end
-		input.each { |rule| output << rule.to_s.split("\s-\s")[0] }
-		normalize(output)
-	end
+  def find_object_group(options)
+    input = Array.new
+    output = Array.new
+    @yaml.each do |line|
+      input << line.match(options[:filter]) if line.match(options[:filter])
+    end
+    input.each { |rule| output << rule.to_s.split("\s-\s")[0] }
+    normalize(output)
+  end
 
-	# Load Cisco 'show all' dump
-	def load_config(filename)
-		@config = File.open(filename, 'r:ascii-8bit')
-		serialize
-		load_yaml
-		clean_up
-	end
+  # Load Cisco 'show all' dump
+  def load_config(filename)
+    @config = File.open(filename, 'r:ascii-8bit')
+    serialize
+    load_yaml
+    clean_up
+  end
 
-	# Load our serialized output
-	def load_yaml
-		@yaml = YAML::load(File.open('tmp'))
-	end
+  # Load our serialized output
+  def load_yaml
+    @yaml = YAML::load(File.open('tmp'))
+  end
 
-	# Remove our tmp file left behind by serialize()
-	def clean_up
-		File.delete('tmp')
-	end
+  # Remove our tmp file left behind by serialize()
+  def clean_up
+    File.delete('tmp')
+  end
 
-	# Normalize output
-	def normalize(input)
-		output = Array.new
-		input.each do |line|
-			output << line.to_s.gsub("\s-\s",'\n')
-		end
-		output
-	end
+  # Normalize output
+  def normalize(input)
+    output = Array.new
+    input.each do |line|
+      output << line.to_s.gsub("\s-\s",'\n')
+    end
+    output
+  end
 
-	# Serialize load_config(), make YAML compatible
-	def serialize
-		yaml = File.open('tmp', 'w')
-		@config.each do |line|
-			case line
-			when /^!/
-				next
-			when /^names/
-				next
-			when /^\S/
-				yaml.puts "\s-\s#{line.gsub("#","*")}"
-			when /^\s/
-				yaml.puts "\s\s-\s#{line}"
-			else
-				# TODO: raise an error here 'woops'
-			end
-		end
-	end
+  # Serialize load_config(), make YAML compatible
+  def serialize
+    yaml = File.open('tmp', 'w')
+    @config.each do |line|
+      case line
+      when /^!/
+        next
+      when /^names/
+        next
+      when /^\S/
+        yaml.puts "\s-\s#{line.gsub("#","*")}"
+      when /^\s/
+        yaml.puts "\s\s-\s#{line}"
+      else
+        # TODO: raise an error here 'woops'
+      end
+    end
+  end
 end
--- spec/iosparse_spec.rb
+++ spec/iosparse_spec.rb
@@ -3,44 +3,44 @@ require 'spec_helper'
 
 describe 'IOSParse' do
 
-	before :all do
-		@config = IOSParse.new('etc/cisco_asa.config')
-	end
+  before :all do
+    @config = IOSParse.new('etc/cisco_asa.config')
+  end
 
-	it 'should be an object type IOSParse' do
-		@config.is_a?(IOSParse)
-	end
+  it 'should be an object type IOSParse' do
+    @config.is_a?(IOSParse)
+  end
 
-	describe 'Find all #{parent}' do
+  describe 'Find all #{parent}' do
 
-		it 'should return an Array object' do
-			@config.find_all('interface').is_a?(Array)
-		end
+    it 'should return an Array object' do
+      @config.find_all('interface').is_a?(Array)
+    end
 
-		it 'should return a list of #{parent}' do
-			@config.find_all('interface').each { |x| false unless x.to_s.match(/^interface/) }
-		end
-	end
+    it 'should return a list of #{parent}' do
+      @config.find_all('interface').each { |x| false unless x.to_s.match(/^interface/) }
+    end
+  end
 
-	describe 'Has #{filter}' do
+  describe 'Has #{filter}' do
 
-		it 'should return an Array object' do
-			@config.has('192.168.1.1').is_a?(Array)
-		end
+    it 'should return an Array object' do
+      @config.has('192.168.1.1').is_a?(Array)
+    end
 
-		it 'should return a list that includes #{filter}' do
-			@config.has('192.168.1.1').each { |x| false unless x.to_s.match(/.+192\.168\.1\.1.+/) }
-		end
-	end
+    it 'should return a list that includes #{filter}' do
+      @config.has('192.168.1.1').each { |x| false unless x.to_s.match(/.+192\.168\.1\.1.+/) }
+    end
+  end
 
-	describe 'Group has #{ip}' do
+  describe 'Group has #{ip}' do
 
-		it 'should return an Array object' do
-			@config.group_has_ip('192.168.5.1').is_a?(Array)
-		end
+    it 'should return an Array object' do
+      @config.group_has_ip('192.168.5.1').is_a?(Array)
+    end
 
-		it 'should return a group(s) that includes #{ip}' do
-			@config.group_has_ip('192.168.5.1').each { |x| false unless x.to_s.match(/^object-group/) }
-		end
-	end
+    it 'should return a group(s) that includes #{ip}' do
+      @config.group_has_ip('192.168.5.1').each { |x| false unless x.to_s.match(/^object-group/) }
+    end
+  end
 end
